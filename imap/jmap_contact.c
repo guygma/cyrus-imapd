2ce103db6c9627faf9eb0f7abcffe51e6d696d9c
Ken Murchison <murch@fastmail.com>
2019-05-28 14:42:46 -0400

    jmap_contact.c: annotatemore_msg_lookup() now uses struct mailbox as its param

--- imap/jmap_contact.c
+++ imap/jmap_contact.c
@@ -1790,7 +1790,7 @@ static json_t *jmap_contact_from_vcard(struct vparse_card *card,
 
     const char *annot = DAV_ANNOT_NS "<" XML_NS_CYRUS ">importance";
     buf_reset(&buf);
-    annotatemore_msg_lookup(mailbox->name, record->uid, annot, "", &buf);
+    annotatemore_msg_lookup(mailbox, record->uid, annot, "", &buf);
     double val = 0;
     if (buf.len) val = strtod(buf_cstring(&buf), NULL);
 
